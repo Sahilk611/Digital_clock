db04b885b5189c89c7fb9d2886a910805ab24a1f
Sahilk611 <138815570+Sahilk611@users.noreply.github.com>
2023-10-22 15:45:30 +0530

    Add files via upload

---
+++ chai.js
@@ -0,0 +1,10 @@
+console.log("sahil khan");
+const clock = document.querySelector('.clock');
+
+
+
+setInterval(function(){
+    let date = new Date();
+    //console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+},1000);
